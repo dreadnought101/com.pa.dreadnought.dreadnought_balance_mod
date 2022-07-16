83e84d13c83049ef35cb75a4fee3126c838e916b
dreadnought101 <michaeljonesrox@gmail.com>
2022-07-16 17:25:50 -0600

    Patch 1.43 Fixes B

--- ui/mods/mla.unit.addon/shared_build.js
+++ ui/mods/mla.unit.addon/shared_build.js
@@ -1,5 +1,5 @@
 var newBuild = {
-    "/pa/units/land/ls_lynx/lynx.json": ["L_vehicle", 0, { row: 1, column: 7, titans: true }]
+    "/pa/units/land/ls_lynx/lynx.json": ["L_vehicle", 0, { row: 2, column: 6, titans: true }]
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
